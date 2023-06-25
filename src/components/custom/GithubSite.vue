f7011070a7e5d034a8a113e81ab8a75ba893038c
ikiwi <ikiwicc@gmail.com>
2023-06-25 14:45:54 +0800

    upload source code firstly

---
+++ src/components/custom/GithubSite.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-neutral-400">
+    <span>View on</span>
+    <a href="https://ikiwi.cc" target="_blank" class="text-blue-500">
+      ikiwi.cc
+    </a>
+  </div>
+</template>
